d35455c47f6b8afc5df688683e375409b75fc103
Ricardo Quezada <qf7.ricardo@gmail.com>
2018-12-12 09:17:10 -0600

    flake8 para estilo de código
    PEP 8 y 256.

--- generar_reporte_mensual.py
+++ generar_reporte_mensual.py
@@ -25,49 +25,57 @@ import sys
 import xml.etree.ElementTree as ET
 
 
-ANCHO_DE_REPORTE = 80
-LLAVES = [{'denominacionPropia': 'fecha',
-           'caminoXML': [
+LLAVES = [
+        {'denominacionPropia': 'fecha',
+         'caminoXML': [
                 'Fecha']},
-           {'denominacionPropia': 'emisor',
-            'caminoXML': [
+        {'denominacionPropia': 'emisor',
+         'caminoXML': [
                 'Emisor',
                 'Nombre']},
-           {'denominacionPropia': 'receptor',
-            'caminoXML': [
+        {'denominacionPropia': 'receptor',
+         'caminoXML': [
                 'Receptor',
                 'Nombre']},
-           {'denominacionPropia': 'subtotal',
-            'caminoXML': [
+        {'denominacionPropia': 'subtotal',
+         'caminoXML': [
                 'SubTotal']},
-           {'denominacionPropia': 'impuestos',
-            'caminoXML': [
+        {'denominacionPropia': 'impuestos',
+         'caminoXML': [
                 'Impuestos',
                 'TotalImpuestosTrasladados']},
-           {'denominacionPropia': 'total',
-            'caminoXML': [
+        {'denominacionPropia': 'total',
+         'caminoXML': [
                 'Total']}]
 
 
-def imprimirItem(contenido, vinieta = '*', ancho = ANCHO_DE_REPORTE):
+def imprimir_item(contenido, vinieta='*', ancho=80):
+    """Imprime el contenido dado como ítem de lista.
 
+    Argumentos:
+    contenido -- cadena a imprimir.
+
+    Argumentos en llaves:
+    vinieta -- cadena a usar como viñeta (por defecto '*').
+    ancho   -- ancho máximo de línea (por defecto 80).
+    """
     contenido = str(contenido)
-    anchoReal = ancho - len(vinieta) - 1
-    numeroDeLineas = (len(contenido) // anchoReal) + 1
-    if len(contenido) % anchoReal == 0:
-        numeroDeLineas -= 1
+    ancho_real = ancho - len(vinieta) - 1
+    numero_de_lineas = (len(contenido) // ancho_real) + 1
+    if len(contenido) % ancho_real == 0:
+        numero_de_lineas -= 1
 
     print(vinieta + ' ', end='')
-    for i in range(numeroDeLineas):
+    for i in range(numero_de_lineas):
         if i != 0:
             print(' ' * (len(vinieta) + 1), end='')
-        if i == numeroDeLineas - 1:
-            print(contenido[i * anchoReal:], ' ' * (len(vinieta) + 1), sep='')
+        if i == numero_de_lineas - 1:
+            print(contenido[i * ancho_real:], ' ' * (len(vinieta) + 1), sep='')
         else:
-            print(contenido[i * anchoReal : (i + 1) * anchoReal])
+            print(contenido[i * ancho_real:(i + 1) * ancho_real])
 
 
-def imprimirTitulo(titulo):
+def imprimir_titulo(titulo):
     """Imprime en la salida estándar el título dado."""
     mitad = (80 - len(titulo) + 2) // 2
     par = 1
@@ -81,32 +89,50 @@ def imprimirTitulo(titulo):
     print('')
 
 
-def imprimirDatos(datos):
+def imprimir_datos(datos):
+    """Imprime el diccionario dado como una lista."""
     for i in datos:
-        imprimirItem(datos[i])
+        imprimir_item(datos[i])
     print('')
 
-def procesarXml(nombreDeArchivo, llaves=LLAVES):
-    """Regresa un diccionario con la información relevante del archivo dado."""
 
-    imprimirItem('Procesando ' + nombreDeArchivo, vinieta='=>')
+def procesar_xml(nombre_de_archivo, llaves=LLAVES):
+    """Regresa un diccionario con la información relevante del archivo dado.
+
+    Cada llave tiene dos elementos: la denominación propia (el nombre que
+    tendrá el campo en el diccionario resultado) y el camino en el
+    XML del SAT (ver buscar_elemento).
+
+    Argumentos:
+    nombre_de_archivo -- ruta al xml fuente.
+
+    Argumentos en llaves:
+    llaves -- Arreglo de diccionarios con la información a buscar.
+    """
+    imprimir_item('Procesando ' + nombre_de_archivo, vinieta='=>')
     print('')
     resultado = {}
-    raiz = ET.parse(nombreDeArchivo).getroot()
+    raiz = ET.parse(nombre_de_archivo).getroot()
     for llave in llaves:
         resultado[llave['denominacionPropia']] = \
-            buscarElemento(raiz, llave['caminoXML'])
-    imprimirDatos(resultado)
+            buscar_elemento(raiz, llave['caminoXML'])
+    imprimir_datos(resultado)
     return resultado
 
 
-def buscarElemento(raiz, camino):
+def buscar_elemento(raiz, camino):
+    """Busca el elemento dado en la raíz xml dada.
+
+    Función recursiva. El caso base es cuando solamente hay un elemento
+    el el arreglo camino; en este caso se regresa el atributo con el
+    nombre dado en la raíz dada. En caso contrario, busca la etiqueta
+    del primer elemento del camino en la raíz dada.
+    """
     if len(camino) == 1:
         return raiz.attrib[camino[0]]
     else:
-        return buscarElemento(raiz.find(
+        return buscar_elemento(raiz.find(
             '{http://www.sat.gob.mx/cfd/3}' + camino[0]), camino[1:])
-
 
 
 if __name__ == '__main__':
@@ -115,16 +141,17 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         carpetaDeFacturas = sys.argv[1]
 
-    imprimirTitulo('Procesamiento')
+    imprimir_titulo('Procesamiento')
     datos = []
     subtotal = impuestos = total = 0
     for archivo in os.listdir(carpetaDeFacturas):
         if archivo[-3:] == 'xml':
-            datos.append(procesarXml(os.path.join(carpetaDeFacturas, archivo)))
+            datos.append(procesar_xml(
+                os.path.join(carpetaDeFacturas, archivo)))
             subtotal += float(datos[-1]['subtotal'])
             impuestos += float(datos[-1]['impuestos'])
             total += float(datos[-1]['total'])
 
-    imprimirTitulo('Resultados')
-    imprimirDatos(
+    imprimir_titulo('Resultados')
+    imprimir_datos(
             {'subtotal': subtotal, 'impuestos': impuestos, 'total': total})
